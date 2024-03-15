ace50fc03af5a92acc2e3adc7b3958b260a70e49
Rony Sarker <ronypk999@gmail.com>
2024-03-15 23:51:44 +0600

    x

--- ipn/deposit.js
+++ ipn/deposit.js
@@ -6,40 +6,44 @@ require("dotenv").config();
 //ooyzahNyYpT97hR6otAHG1tpu2Czuh2j
 module.exports = {
   deposit: async function (req) {
-    const hmac = crypto.createHmac("sha512", process.env.NOW_IPN);
-    hmac.update(JSON.stringify(this.sortObject(req.body)));
-    const signature = hmac.digest("hex");
+    try {
+      const hmac = crypto.createHmac("sha512", process.env.NOW_IPN);
+      hmac.update(JSON.stringify(this.sortObject(req.body)));
+      const signature = hmac.digest("hex");
 
-    if (signature === req.headers["x-nowpayments-sig"]) {
-      const data = req.body;
-      const getDeposit = await deposit.find({ payment_id: data.payment_id });
-      if (
-        data.payment_status === "finished" &&
-        getDeposit[0].payment_status !== "finished"
-      ) {
-        const actuallyPaidDecimal = mongoose.Types.Decimal128.fromString(
-          parseFloat(data.actually_paid).toString()
-        );
-        await user.findByIdAndUpdate(getDeposit[0].user_id, {
-          $inc: {
-            [`balance.${getDeposit[0].pay_currency}`]: actuallyPaidDecimal,
-          },
-        });
-        await deposit.findByIdAndUpdate(getDeposit[0]._id, {
-          payment_status: data.payment_status,
-          pay_amount: data.actually_paid,
-        });
-      }
+      if (signature === req.headers["x-nowpayments-sig"]) {
+        const data = req.body;
+        const getDeposit = await deposit.find({ payment_id: data.payment_id });
+        if (
+          data.payment_status === "finished" &&
+          getDeposit[0].payment_status !== "finished"
+        ) {
+          const actuallyPaidDecimal = mongoose.Types.Decimal128.fromString(
+            parseFloat(data.actually_paid).toString()
+          );
+          await user.findByIdAndUpdate(getDeposit[0].user_id, {
+            $inc: {
+              [`balance.${getDeposit[0].pay_currency}`]: actuallyPaidDecimal,
+            },
+          });
+          await deposit.findByIdAndUpdate(getDeposit[0]._id, {
+            payment_status: data.payment_status,
+            pay_amount: data.actually_paid,
+          });
+        }
 
-      if (
-        data.payment_status === "refunded" ||
-        data.payment_status === "failed" ||
-        data.payment_status === "expired"
-      ) {
-        await deposit.findByIdAndUpdate(getDeposit[0]._id, {
-          payment_status: "failed",
-        });
+        if (
+          data.payment_status === "refunded" ||
+          data.payment_status === "failed" ||
+          data.payment_status === "expired"
+        ) {
+          await deposit.findByIdAndUpdate(getDeposit[0]._id, {
+            payment_status: "failed",
+          });
+        }
       }
+    } catch (error) {
+      console.log(error);
     }
   },
   sortObject: function (obj) {
