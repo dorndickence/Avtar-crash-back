9df1df4e71d2fece108d756fc30a32fe551d4cac
Dornavan <130479724+dorndickence@users.noreply.github.com>
2025-01-03 17:42:57 +0300

    Update deposit.js

--- ipn/deposit.js
+++ ipn/deposit.js
@@ -3,7 +3,7 @@ const deposit = require("../model/deposit");
 const crypto = require("crypto");
 const { mongoose } = require("mongoose");
 require("dotenv").config();
-//ooyzahNyYpT97hR6otAHG1tpu2Czuh2j
+
 module.exports = {
   deposit: async function (req) {
     try {
@@ -11,41 +11,43 @@ module.exports = {
       hmac.update(JSON.stringify(this.sortObject(req.body)));
       const signature = hmac.digest("hex");
 
+      // Verify the signature
       if (signature === req.headers["x-nowpayments-sig"]) {
         const data = req.body;
         const getDeposit = await deposit.find({ payment_id: data.payment_id });
-        if (
-          data.payment_status === "finished" &&
-          getDeposit[0].payment_status !== "finished"
-        ) {
+
+        // Check payment status and update accordingly
+        if (data.payment_status === "finished" && getDeposit.length > 0 && getDeposit[0].payment_status !== "finished") {
           const actuallyPaidDecimal = mongoose.Types.Decimal128.fromString(
             parseFloat(data.actually_paid).toString()
           );
+
+          // Update user balance
           await user.findByIdAndUpdate(getDeposit[0].user_id, {
             $inc: {
               [`balance.${getDeposit[0].pay_currency}`]: actuallyPaidDecimal,
             },
           });
+
+          // Update deposit record
           await deposit.findByIdAndUpdate(getDeposit[0]._id, {
             payment_status: data.payment_status,
             pay_amount: data.actually_paid,
           });
         }
 
-        if (
-          data.payment_status === "refunded" ||
-          data.payment_status === "failed" ||
-          data.payment_status === "expired"
-        ) {
+        // Handle failed or refunded payments
+        if (["refunded", "failed", "expired"].includes(data.payment_status)) {
           await deposit.findByIdAndUpdate(getDeposit[0]._id, {
             payment_status: "failed",
           });
         }
       }
     } catch (error) {
-      console.log(error);
+      console.error("Error processing deposit:", error);
     }
   },
+
   sortObject: function (obj) {
     return Object.keys(obj)
       .sort()
